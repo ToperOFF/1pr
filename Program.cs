30d1741510cd1919f8d43adc5058ea6477b7c159
Pavel Krashchnko <103641777+ToperOFF@users.noreply.github.com>
2022-04-14 15:25:53 +0300

    Написан метод Main

--- Program.cs
+++ Program.cs
@@ -10,8 +10,20 @@ namespace _1pr
     {
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Введите наименование: ");
+            string name = Console.ReadLine();
 
+            Console.WriteLine("Введите количество: ");
+            int kolvo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите стоимость: ");
+            float stoimost = (float)Convert.ToDouble(Console.ReadLine());
+
+            TovarClass TC = new TovarClass(name, kolvo, stoimost);
+
+            Console.WriteLine(TC.Name + " " + TC.Kolvo + " " + TC.Stoimost);
+
+        }
     }
     class TovarClass
     {
